2451ac325f68141363c5288bab9ebbd7ed817707
Gaëtan Denis <gaetandenis@outlook.be>
2023-09-29 09:41:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!--Reset CSS-->
+    <link rel="stylesheet" href="styles/reset.css">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<div class="grit-container">
+    <div class="header">
+        <h1>feuille de personnage</h1>
+    </div>
+    <img src="img/avatar.png" alt="">
+    <div class="informations-container">
+        <div class="main-informations">
+            <h2>informations générales</h2>
+            <ul>
+                <li>Nom/Prénom (dans cet ordre) :</li>
+                <li>Date de naissance :</li>
+                <li>Taille :</li>
+                <li>Groupe sanguin (A, B, AB, O ou S) :</li>
+                <li>Métier :</li>
+                <li>Province :</li>
+            </ul>
+        </div>
+        <div class="experience">
+            <h2>experience</h2>
+            <ul>
+                <li>Pc totaux :</li>
+                <li>Pc actuels :</li>
+                <li>Pex :</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="traits-aversions-container">
+        <div class="traits">
+            <h2>traits</h2>
+            <ul>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+            </ul>
+        </div>
+        <div class="aversions">
+            <h2>aversions</h2>
+            <ul>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+            </ul>
+        </div>
+    </div>
+    <div class="peculiarities">
+        <h2>particularités</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto expedita ipsa magnam ullam velit veritatis! Architecto assumenda doloribus exercitationem quibusdam! Aspernatur autem eligendi nulla sed tempora unde veniam, voluptates. Ab accusamus ad alias architecto at blanditiis commodi culpa eaque eius error esse est eum eveniet facilis hic id illum, incidunt inventore ipsam itaque iure maiores nam nesciunt nihil nobis numquam, odio odit quaerat reiciendis, repudiandae rerum suscipit tempora totam ullam veniam vero voluptas! A animi aperiam dignissimos excepturi fuga odio officiis porro sequi sunt ut! Amet aperiam architecto beatae blanditiis consectetur cupiditate distinctio dolor doloribus ducimus ea esse eveniet exercitationem expedita illo inventore ipsa ipsam itaque magni minus modi nobis nulla quis repellat sed, sequi similique sint sit suscipit tempore tenetur totam ut voluptates voluptatum. A adipisci architecto assumenda dolores ducimus fuga harum laboriosam magnam maiores nesciunt nulla omnis repellat reprehenderit sequi sint veniam, voluptates. A in nisi reiciendis rem repellendus tenetur! Adipisci culpa earum ipsum labore nulla quae quia, quisquam reprehenderit tempore veniam! Aspernatur at doloribus esse facere nostrum repellat saepe sapiente temporibus veritatis. Alias deleniti nisi obcaecati officia ratione! Architecto eos natus nesciunt quia ut. Ab accusamus ad commodi cupiditate, dolore dolores ea excepturi expedita hic illo inventore magnam minima nemo nesciunt praesentium provident qui quia quis repellat repudiandae ut vero voluptatibus. Dicta doloremque dolorum facere minima necessitatibus perferendis placeat quis temporibus vitae voluptatum. Enim expedita, in nemo officia quae rem repellat unde veritatis voluptatum? Animi eos fugit ipsam magnam nisi nostrum, odio placeat possimus quidem repellat sapiente.</p>
+    </div>
+    <div class="possessions">
+        <h2>possession</h2>
+        <p></p>
+    </div>
+    <div class="contacts">
+        <h2>contact</h2>
+        <p></p>
+    </div>
+    <div class="diary">
+        <h2>journal</h2>
+        <p></p>
+    </div>
+</div>
+
+</body>
+</html>
